695281da3412174df5f595be950f3cebd491122c
Luis <lperezsilva1@gmail.com>
2025-10-01 20:22:40 -0500

    se agrego un control de permisos y roles 3

--- resources/views/rbac/roles.blade.php
+++
@@ -1,7 +0,0 @@
-<x-settings.rbac
-  heading="Roles"
-  subheading="Create, rename and delete roles"
-  contentClass="mt-5 w-full"  {{-- ancho completo para la tabla --}}
->
-  <livewire:settings.roles-index />
-</x-settings.rbac>
