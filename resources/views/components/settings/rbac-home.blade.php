7e3f4112a95ee12757f621d83a90d3f33bd5d597
Luis <lperezsilva1@gmail.com>
2025-10-01 20:01:25 -0500

    se agrego un control de permisos y roles 2

--- resources/views/components/settings/rbac-home.blade.php
+++
@@ -1,7 +0,0 @@
-<x-settings.rbac
-  heading="Roles & Permissions"
-  subheading="Manage roles, permissions and role assignment"
-  contentClass="mt-5 w-full"
->
-  {{-- Intencionalmente vacío: solo el wrapper --}}
-</x-settings.rbac>
